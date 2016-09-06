5a7afed18f0a9b35b6dba71e4aa853abd05ff102
Alex Watson <awatson347@aol.com>
2016-09-06 01:35:17 -0400

    update

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '	3wPJPlIPG6G2e0TlNfhnYZZlS',
+  consumer_secret: 'WqnzXnxWg7Ohd2TnpQ8pLgXHAlzlJEL9Y4CkR1i82vqfIhDFp6',
+  access_token_key: '773025095669297152-Ijap28aGyQCiqVrvHqeP6tAULvz0UZW',
+  access_token_secret: '	uW0d7a5MDlCSQeLnrbXRzKoqWeNM272L9K7gSwAHI5ARw',
 }
